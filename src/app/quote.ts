5a5b39395953c9c9417f167d440910d6e3b8300f
FrancisFlow <iamngigikaranja1@aol.com>
2021-11-21 13:19:56 +0300

    Created the quote blueprint class for creating quote objects

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,13 @@
 export class Quote {
+    constructor(
+        public aQuote:string, 
+        public author:string,
+        public iSubmit:string,
+        
+        public upVote:number,
+        public downVote:number,
+        public dayOfPosting: Date
+
+    ){}
+
 }
