195e15a01245efc7b010c913fdfc3dfd8eceb687
FrancisFlow <iamngigikaranja1@aol.com>
2021-11-22 10:40:34 +0300

    Created pipe to show days passed and chained it to quote.dayOfPosting

---
+++ src/app/days-passed.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'daysPassed'
+})
+export class DaysPassedPipe implements PipeTransform {
+
+  transform(value:any) :number{
+
+    let today:any = new Date();
+    let dateDifference = Math.abs(Date.parse(today)-Date.parse(value))*0.001; //convert milliseconds to seconds
+    let differenceInDays= dateDifference/(60*60*24);  //get time in days
+    return Math.round(differenceInDays);
+
+  }
+
+}
